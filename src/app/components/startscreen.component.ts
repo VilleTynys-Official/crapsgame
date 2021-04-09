6f9678f5090de2682cf2aeab74e8b502084d9222
VilleTynysCloubi <ville.tynys@cloubi.com>
2021-04-09 18:44:28 +0300

    Create startscreen

--- src/app/components/startscreen.component.ts
+++ src/app/components/startscreen.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {GameMechanics} from '../services/gamemechanics.service';
 import {Router} from '@angular/router';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
 	selector:"startscreen",
@@ -8,10 +9,21 @@ import {Router} from '@angular/router';
 })
 export class StartScreen {
 
+    userName:string = "";
+	bets: Array<string>= ["PASS", "DONT_PASS"]
+	bet:string = "PASS"
+
 	constructor(private _game:GameMechanics,private _router:Router) {}
 
+	// FOR THE SELECTION
+	changeBet(e) {
+		 this.bet = e.target.value
+		 console.log('bet is', this.bet);
+		 
+	}
+
 	startGame() {
-		this._game.startGame();
+		this._game.startGame(this.userName, this.bet);
 		this._router.navigate(['/game']);
 	}
 }
